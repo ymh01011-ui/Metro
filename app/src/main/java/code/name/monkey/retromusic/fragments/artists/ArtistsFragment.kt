61ec210bbe36a9c1773d4eebceac84943b64f7ac
ymh01011-ui <ymh01011@gmail.com>
2026-09-12 19:04:13 +0300

    Refactor ArtistsFragment for improved functionality

--- app/src/main/java/code/name/monkey/retromusic/fragments/artists/ArtistsFragment.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/artists/ArtistsFragment.kt
@@ -15,123 +15,374 @@
 package code.name.monkey.retromusic.fragments.artists
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
+import android.view.*
 import androidx.core.os.bundleOf
-import androidx.navigation.findNavController
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import code.name.monkey.retromusic.EXTRA_ARTIST_ID
+import code.name.monkey.retromusic.EXTRA_ARTIST_NAME
 import code.name.monkey.retromusic.R
 import code.name.monkey.retromusic.adapter.artist.ArtistAdapter
-import code.name.monkey.retromusic.databinding.FragmentArtistsBinding
-import code.name.monkey.retromusic.extensions.accentColor
-import code.name.monkey.retromusic.extensions.dip
-import code.name.monkey.retromusic.extensions.filterByExtraArtist
-import code.name.monkey.retromusic.extensions.saveSortArtistTo
-import code.name.monkey.retromusic.fragments.base.AbsLibraryPagerRecyclerViewFragment
-import code.name.monkey.retromusic.helper.SortOrder
+import code.name.monkey.retromusic.fragments.GridStyle
+import code.name.monkey.retromusic.fragments.ReloadType
+import code.name.monkey.retromusic.fragments.base.AbsRecyclerViewCustomGridSizeFragment
+import code.name.monkey.retromusic.helper.MusicPlayerRemote
+import code.name.monkey.retromusic.helper.SortOrder.ArtistSortOrder
+import code.name.monkey.retromusic.interfaces.IAlbumArtistClickListener
 import code.name.monkey.retromusic.interfaces.IArtistClickListener
-import code.name.monkey.retromusic.model.Artist
+import code.name.monkey.retromusic.interfaces.IMultiArtistClickListener
+import code.name.monkey.retromusic.service.MusicService
 import code.name.monkey.retromusic.util.PreferenceUtil
-import com.google.android.material.transition.MaterialSharedAxisTransition
-import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import code.name.monkey.retromusic.util.RetroUtil
 
-class ArtistsFragment : AbsLibraryPagerRecyclerViewFragment<ArtistAdapter, GridLayoutManager>(),
-    IArtistClickListener {
-    private val libraryViewModel: LibraryViewModel by sharedViewModel()
-    private var _binding: FragmentArtistsBinding? = null
-    private val binding get() = _binding!!
-
+class ArtistsFragment : AbsRecyclerViewCustomGridSizeFragment<ArtistAdapter, GridLayoutManager>(),
+    IArtistClickListener, IAlbumArtistClickListener, IMultiArtistClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentArtistsBinding.bind(view)
-        enterTransition = MaterialSharedAxisTransition(MaterialSharedAxisTransition.Y_AXIS, true)
-        exitTransition = MaterialSharedAxisTransition(MaterialSharedAxisTransition.Y_AXIS, false)
-        libraryViewModel.artists.observe(viewLifecycleOwner) {
+        libraryViewModel.getArtists().observe(viewLifecycleOwner) {
             if (it.isNotEmpty())
-                onArtistsLoaded(it)
+                adapter?.swapDataSet(it)
             else
-                showEmptyView()
+                adapter?.swapDataSet(listOf())
         }
     }
 
-    private fun onArtistsLoaded(artists: List<Artist>) {
-        val extraArtistName = arguments?.getString(EXTRA_ARTIST_NAME, null)
-        val filtered = artists.filterByExtraArtist(extraArtistName)
-        adapter?.swapData(filtered)
-    }
+    override val titleRes: Int
+        get() = R.string.artists
 
-    override fun onPrepareMenu(menu: Menu) {
-        super.onPrepareMenu(menu)
-        menu.findItem(R.id.action_grid_size)?.isVisible = true
-        menu.findItem(R.id.action_layout_type)?.isVisible = true
-        menu.findItem(R.id.action_sort_order)?.isVisible = true
-    }
+    override val emptyMessage: Int
+        get() = R.string.no_artists
 
-    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-        if (handleSortOrderMenuItem(menuItem)) {
-            return true
+    override val isShuffleVisible: Boolean
+        get() = true
+
+    override fun onShuffleClicked() {
+        libraryViewModel.getArtists().value?.let {
+            MusicPlayerRemote.setShuffleMode(MusicService.SHUFFLE_MODE_NONE)
+            MusicPlayerRemote.openQueue(
+                queue = it.shuffled().flatMap { artist -> artist.songs },
+                startPosition = 0,
+                startPlaying = true
+            )
         }
-        return super.onMenuItemSelected(menuItem)
     }
 
-    private fun handleSortOrderMenuItem(item: MenuItem): Boolean {
-        val sortOrder: String = when (item.itemId) {
-            R.id.action_sort_order_asc -> SortOrder.ArtistSortOrder.ARTIST_A_Z
-            R.id.action_sort_order_desc -> SortOrder.ArtistSortOrder.ARTIST_Z_A
-            R.id.action_sort_order_artist_song_count -> SortOrder.ArtistSortOrder.ARTIST_NUMBER_OF_SONGS
-            R.id.action_sort_order_artist_album_count -> SortOrder.ArtistSortOrder.ARTIST_NUMBER_OF_ALBUMS
-            else -> return false
-        }
-        item.isChecked = true
-        PreferenceUtil.artistSortOrder = sortOrder
-        saveSortArtistTo(sortOrder)
-        return true
+    override fun setSortOrder(sortOrder: String) {
+        libraryViewModel.forceReload(ReloadType.Artists)
     }
 
     override fun createLayoutManager(): GridLayoutManager {
-        return GridLayoutManager(
-            requireActivity(),
-            getGridSize(),
-            GridLayoutManager.VERTICAL,
-            false
-        )
+        return GridLayoutManager(requireActivity(), getGridSize())
     }
 
     override fun createAdapter(): ArtistAdapter {
+        val dataSet = if (adapter == null) ArrayList() else adapter!!.dataSet
         return ArtistAdapter(
             requireActivity(),
-            emptyList(),
-            PreferenceUtil.artistGridStyle,
+            dataSet,
+            itemLayoutRes(),
             this,
-            null,
-            null
+            this,
+            this
         )
+    }
+
+    override fun loadGridSize(): Int {
+        return PreferenceUtil.artistGridSize
+    }
+
+    override fun saveGridSize(gridColumns: Int) {
+        PreferenceUtil.artistGridSize = gridColumns
+    }
+
+    override fun loadGridSizeLand(): Int {
+        return PreferenceUtil.artistGridSizeLand
+    }
+
+    override fun saveGridSizeLand(gridColumns: Int) {
+        PreferenceUtil.artistGridSizeLand = gridColumns
+    }
+
+    override fun setGridSize(gridSize: Int) {
+        layoutManager?.spanCount = gridSize
+        adapter?.notifyDataSetChanged()
+    }
+
+    override fun loadSortOrder(): String {
+        return PreferenceUtil.artistSortOrder
+    }
+
+    override fun saveSortOrder(sortOrder: String) {
+        PreferenceUtil.artistSortOrder = sortOrder
+    }
+
+    override fun loadLayoutRes(): Int {
+        return PreferenceUtil.artistGridStyle.layoutResId
+    }
+
+    override fun saveLayoutRes(layoutRes: Int) {
+        PreferenceUtil.artistGridStyle = GridStyle.values().first { gridStyle ->
+            gridStyle.layoutResId == layoutRes
+        }
+    }
+
+    companion object {
+
+        fun newInstance(): ArtistsFragment {
+            return ArtistsFragment()
+        }
     }
 
     override fun onArtist(artistId: Long, view: View) {
-        exitTransition = MaterialSharedAxisTransition(MaterialSharedAxisTransition.Y_AXIS, false)
-        requireView().findNavController().navigate(
+        // نفس الـ anim resources بتاعة صفحة "See All Songs" - أنيميشن سلايد
+        // بسيط بدل الـ Container Transform (shared element) القديم.
+        val navOptions = NavOptions.Builder()
+            .setEnterAnim(R.anim.nav_slide_in_right)
+            .setExitAnim(R.anim.nav_slide_out_left)
+            .setPopEnterAnim(R.anim.nav_slide_in_left)
+            .setPopExitAnim(R.anim.nav_slide_out_right)
+            .build()
+
+        findNavController().navigate(
             R.id.artistDetailsFragment,
             bundleOf(EXTRA_ARTIST_ID to artistId),
-            null,
-            FragmentNavigatorExtras(view to artistId.toString())
+            navOptions
         )
+        reenterTransition = null
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onAlbumArtist(artistName: String, view: View) {
+        findNavController().navigate(
+            R.id.albumArtistDetailsFragment,
+            bundleOf(EXTRA_ARTIST_NAME to artistName),
+            null,
+            FragmentNavigatorExtras(view to artistName)
+        )
+        reenterTransition = null
     }
 
-    override fun getEmptyStateDrawableRes(): Int = R.drawable.ic_empty_music
-    override fun getEmptyStateTextRes(): Int = R.string.no_artists
+    override fun onMultiArtist(artistName: String, view: View) {
+        findNavController().navigate(
+            R.id.multiArtistDetailsFragment,
+            bundleOf(EXTRA_ARTIST_NAME to artistName),
+            null,
+            FragmentNavigatorExtras(view to artistName)
+        )
+        reenterTransition = null
+    }
 
-    companion object {
-        const val TAG: String = "ArtistsFragment"
-        private const val EXTRA_ARTIST_ID = "extra_artist_id"
-        private const val EXTRA_ARTIST_NAME = "extra_artist_name"
+    override fun onCreateMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateMenu(menu, inflater)
+        val gridSizeItem: MenuItem = menu.findItem(R.id.action_grid_size)
+        if (RetroUtil.isLandscape) {
+            gridSizeItem.setTitle(R.string.action_grid_size_land)
+        }
+        setUpGridSizeMenu(gridSizeItem.subMenu!!)
+        val layoutItem = menu.findItem(R.id.action_layout_type)
+        setupLayoutMenu(layoutItem.subMenu!!)
+        setUpSortOrderMenu(menu.findItem(R.id.action_sort_order).subMenu!!)
+        setupAlbumArtistMenu(menu)
+        setupMultiArtistMenu(menu)
+    }
+
+    private fun setupAlbumArtistMenu(menu: Menu) {
+        menu.add(0, R.id.action_album_artist, 0, R.string.show_album_artists).apply {
+            isCheckable = true
+            isChecked = PreferenceUtil.albumArtistsOnly
+        }
+    }
+
+    private fun setupMultiArtistMenu(menu: Menu) {
+        menu.add(0, R.id.action_multi_artist, 0, "Split multiple artists").apply {
+            isCheckable = true
+            isChecked = PreferenceUtil.multiArtistsEnabled
+        }
+    }
+
+    private fun setUpSortOrderMenu(
+        sortOrderMenu: SubMenu
+    ) {
+        val currentSortOrder: String? = getSortOrder()
+        sortOrderMenu.clear()
+        sortOrderMenu.add(
+            0,
+            R.id.action_artist_sort_order_asc,
+            0,
+            R.string.sort_order_a_z
+        ).isChecked = currentSortOrder.equals(ArtistSortOrder.ARTIST_A_Z)
+        sortOrderMenu.add(
+            0,
+            R.id.action_artist_sort_order_desc,
+            1,
+            R.string.sort_order_z_a
+        ).isChecked = currentSortOrder.equals(ArtistSortOrder.ARTIST_Z_A)
+        sortOrderMenu.setGroupCheckable(0, true, true)
+    }
+
+    private fun setupLayoutMenu(
+        subMenu: SubMenu
+    ) {
+        when (itemLayoutRes()) {
+            R.layout.item_card -> subMenu.findItem(R.id.action_layout_card).isChecked = true
+            R.layout.item_grid -> subMenu.findItem(R.id.action_layout_normal).isChecked = true
+            R.layout.item_card_color -> subMenu.findItem(R.id.action_layout_colored_card).isChecked =
+                true
+            R.layout.item_grid_circle -> subMenu.findItem(R.id.action_layout_circular).isChecked =
+                true
+            R.layout.image -> subMenu.findItem(R.id.action_layout_image).isChecked = true
+            R.layout.item_image_gradient -> subMenu.findItem(R.id.action_layout_gradient_image).isChecked =
+                true
+        }
+    }
+
+    private fun setUpGridSizeMenu(
+        gridSizeMenu: SubMenu
+    ) {
+        when (getGridSize()) {
+            1 -> gridSizeMenu.findItem(R.id.action_grid_size_1).isChecked =
+                true
+            2 -> gridSizeMenu.findItem(R.id.action_grid_size_2).isChecked = true
+            3 -> gridSizeMenu.findItem(R.id.action_grid_size_3).isChecked = true
+            4 -> gridSizeMenu.findItem(R.id.action_grid_size_4).isChecked = true
+            5 -> gridSizeMenu.findItem(R.id.action_grid_size_5).isChecked = true
+            6 -> gridSizeMenu.findItem(R.id.action_grid_size_6).isChecked = true
+            7 -> gridSizeMenu.findItem(R.id.action_grid_size_7).isChecked = true
+            8 -> gridSizeMenu.findItem(R.id.action_grid_size_8).isChecked = true
+        }
+        val gridSize: Int = maxGridSize
+        if (gridSize < 8) {
+            gridSizeMenu.findItem(R.id.action_grid_size_8).isVisible = false
+        }
+        if (gridSize < 7) {
+            gridSizeMenu.findItem(R.id.action_grid_size_7).isVisible = false
+        }
+        if (gridSize < 6) {
+            gridSizeMenu.findItem(R.id.action_grid_size_6).isVisible = false
+        }
+        if (gridSize < 5) {
+            gridSizeMenu.findItem(R.id.action_grid_size_5).isVisible = false
+        }
+        if (gridSize < 4) {
+            gridSizeMenu.findItem(R.id.action_grid_size_4).isVisible = false
+        }
+        if (gridSize < 3) {
+            gridSizeMenu.findItem(R.id.action_grid_size_3).isVisible = false
+        }
+    }
+
+    override fun onMenuItemSelected(item: MenuItem): Boolean {
+        if (handleGridSizeMenuItem(item)) {
+            return true
+        }
+        if (handleLayoutResType(item)) {
+            return true
+        }
+        if (handleSortOrderMenuItem(item)) {
+            return true
+        }
+        if (handleAlbumArtistMenu(item)) {
+            return true
+        }
+        if (handleMultiArtistMenu(item)) {
+            return true
+        }
+        return super.onMenuItemSelected(item)
+    }
+
+    private fun handleAlbumArtistMenu(item: MenuItem): Boolean {
+        return if (item.itemId == R.id.action_album_artist) {
+            val newValue = !item.isChecked
+            PreferenceUtil.albumArtistsOnly = newValue
+            item.isChecked = newValue
+            if (newValue) {
+                // Mutually exclusive with multi-artist mode
+                PreferenceUtil.multiArtistsEnabled = false
+            }
+            libraryViewModel.forceReload(ReloadType.Artists)
+            true
+        } else {
+            false
+        }
+    }
+
+    private fun handleMultiArtistMenu(item: MenuItem): Boolean {
+        return if (item.itemId == R.id.action_multi_artist) {
+            val newValue = !item.isChecked
+            PreferenceUtil.multiArtistsEnabled = newValue
+            item.isChecked = newValue
+            if (newValue) {
+                // Mutually exclusive with album-artist mode
+                PreferenceUtil.albumArtistsOnly = false
+            }
+            libraryViewModel.forceReload(ReloadType.Artists)
+            true
+        } else {
+            false
+        }
+    }
+
+    private fun handleSortOrderMenuItem(
+        item: MenuItem
+    ): Boolean {
+        val sortOrder: String = when (item.itemId) {
+            R.id.action_artist_sort_order_asc -> ArtistSortOrder.ARTIST_A_Z
+            R.id.action_artist_sort_order_desc -> ArtistSortOrder.ARTIST_Z_A
+            else -> PreferenceUtil.artistSortOrder
+        }
+        if (sortOrder != PreferenceUtil.artistSortOrder) {
+            item.isChecked = true
+            setAndSaveSortOrder(sortOrder)
+            return true
+        }
+        return false
+    }
+
+    private fun handleLayoutResType(
+        item: MenuItem
+    ): Boolean {
+        val layoutRes = when (item.itemId) {
+            R.id.action_layout_normal -> R.layout.item_grid
+            R.id.action_layout_card -> R.layout.item_card
+            R.id.action_layout_colored_card -> R.layout.item_card_color
+            R.id.action_layout_circular -> R.layout.item_grid_circle
+            R.id.action_layout_image -> R.layout.image
+            R.id.action_layout_gradient_image -> R.layout.item_image_gradient
+            else -> PreferenceUtil.artistGridStyle.layoutResId
+        }
+        if (layoutRes != PreferenceUtil.artistGridStyle.layoutResId) {
+            item.isChecked = true
+            setAndSaveLayoutRes(layoutRes)
+            return true
+        }
+        return false
+    }
+
+    private fun handleGridSizeMenuItem(
+        item: MenuItem
+    ): Boolean {
+        val gridSize = when (item.itemId) {
+            R.id.action_grid_size_1 -> 1
+            R.id.action_grid_size_2 -> 2
+            R.id.action_grid_size_3 -> 3
+            R.id.action_grid_size_4 -> 4
+            R.id.action_grid_size_5 -> 5
+            R.id.action_grid_size_6 -> 6
+            R.id.action_grid_size_7 -> 7
+            R.id.action_grid_size_8 -> 8
+            else -> 0
+        }
+        if (gridSize > 0) {
+            item.isChecked = true
+            setAndSaveGridSize(gridSize)
+            return true
+        }
+        return false
+    }
+
+    override fun onResume() {
+        super.onResume()
+        libraryViewModel.forceReload(ReloadType.Artists)
     }
 }
